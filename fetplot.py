3f0d39d4933a3bb5d75483c2d98988876b9b3f52
Leo Browning <leobrowning92@gmail.com>
2017-07-25 16:36:36 +1200

    corrected errors and refined regex in helper functions
    plus some cosmetic changes

--- fetplot.py
+++ fetplot.py
@@ -6,45 +6,58 @@ Script contains functions for the plotting of data as collected
 by my automated Parameter analyser scripts
 """
 
-import glob, re, os, sys, time, argparse
+import glob, re, os, sys, time, argparse, hashlib
 import matplotlib.pyplot as plt
 from IPython import embed
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from plotting_fns import format_primary_axis,checkdir
+############## COLORS ###################################
 
+divergent=sns.color_palette(["#3778bf", "#feb308", "#441830", "#c13838", "#2f6830"])
+hls=sns.color_palette("hls", 8)
 ##############   Helper Functions   #####################
 # Functions for pulling important information out of filename strings
 def find_deptime(fname):
     """returns the deptime in minutes from the filename"""
     if 'hrdep' in fname:
-        return int(re.search('(\d)hrdep', fname, flags=re.IGNORECASE).group(1))*60
+        return int(re.search('(\d{1,3})hrdep', fname, flags=re.IGNORECASE).group(1))*60
     elif 'mindep' in fname:
-        return int(re.search('(\d)mindep', fname, flags=re.IGNORECASE).group(1))
-    else: return np.nan
+        return int(re.search('(\d{1,3})mindep', fname, flags=re.IGNORECASE).group(1))
+    else: return "deptimeERROR"
 
 def find_parameters(fname):
-    try:return re.search('(VG\d*\dVDS\d*.\d*)',fname).group(1)
-    except: return np.nan
+    try:
+        if 'transfer' in fname:
+            return re.search('(VG[0-9]\d*(:?\.\d+)?VDS[0-9]\d*(:?\.\d+)?)',fname).group(1)
+        if 'output' in fname:
+            return re.search('(VDS[0-9]\d*(:?\.\d+)?VG[0-9]\d*(:?\.\d+)?)',fname).group(1)
+    except: return "parameterERROR"
 
 def find_fabstep(fname):
     try:return re.search('dep(.[^_]*)',fname).group(1)
-    except:return np.nan
+    except:return "fabstepERROR"
 
 def find_numsweeps(fname):
     try:return re.search('x(\d*)',fname).group(1)
     except: return 1
 
 def get_info(df):
-    print("dataframe contains {} datapoints across {} runs".format(df.shape[0],len(df.index.levels[0])))
+    print("dataframe contains {} datapoints across {} runs".format(df.shape[0],len(set(df['uuid']))))
     print("the columns are: \n{}".format(list(df)))
-
+    print(df[:10])
 def get_runID(fname):
-    return fname[-19:-5].replace('_','')
+    return hashlib.md5(fname.encode()).hexdigest()[:8]
+def get_timestamp(fname):
+    try:return re.search('(\d{4}_\d{2}_\d{2}_\d{4})', fname).group(1)
+    except: return "timestampERROR"
+def find_gate(fname):
+    try:return re.search('_([^_]*gate[^_]*)_', fname).group(1)
+    except: return "backgate"
 
 
-def plotchip(directory,number,v=True,display=True,save=True):
+def plotchip(directory,number,v=True,show=True,save=True):
     """
     This function simply plots all data from a chip, and whacks it in seperate
     plots inside a figure. The figures are saved individually in the
@@ -58,10 +71,10 @@ def plotchip(directory,number,v=True,display=True,save=True):
     fnames.sort()
     if v:
         print("plotting {} datasets for chip {}".format(len(fnames),number))
-    fig = plt.figure(figsize=(30, 4), facecolor="white")
+    fig = plt.figure(figsize=(16, 4), facecolor="white")
     axes=[]
     for i in range(len(fnames)):
-        axes.append(plt.subplot(len(fnames)//6,len(fnames)//1,i+1))
+        axes.append(plt.subplot(len(fnames)//6,len(fnames)//2,i+1))
 
     for i in range(len(fnames)):
         if "output" in fnames[i]:
@@ -76,7 +89,7 @@ def plotchip(directory,number,v=True,display=True,save=True):
     checkdir(os.path.join(directory,"plots"))
     if save:
         plt.savefig("{}plots/COL{}_plot.png".format(directory,number))
-    if display:
+    if show:
         plt.show()
 
 def plot_all(directory, start, stop, search,display=True,save=False):
@@ -136,7 +149,7 @@ def plot_all_prepost(predirectory, postdirectory, search, display= True, save=Tr
 
 ####Tiling code
 def load_to_dataframe(directory,v=True):
-    fnames = glob.glob(os.path.join(directory[0],"data/*"))
+    fnames = glob.glob(os.path.join(directory,"data/*"))
     if len(directory)>1:
         for i in range(1,len(directory)):
             fnames=fnames+glob.glob(os.path.join(directory[i],"data/*"))
@@ -145,23 +158,28 @@ def load_to_dataframe(directory,v=True):
     # process fnames
     frames= [ pd.read_csv(fname) for fname in fnames ]
     # sets index to fname and concatenates dataset
+
     df=pd.concat(frames,keys=[os.path.basename(fname) for fname in fnames])
     # moves the fname to a column
-    df['fname']=df.index
+    df['temp']=df.index
 
 
     #makes columns for the various parameters held in the filename
-    df['fname']=df.fname.apply(lambda x:x[0])
-    df['chip']= df['fname'].apply(lambda x:int(x[3:6]) )
-    df['device']= df['fname'].apply(lambda x:x[3:9] )
-    df['deptime']=df['fname'].apply(find_deptime)
-    df['fabstep'] = df['fname'].apply(find_fabstep)
-    df['parameters'] = df['fname'].apply(find_parameters)
-    df['multi'] = df['fname'].apply(find_numsweeps)
-    df.drop(['fname'],axis=1,inplace=True)
-    # # resets the index to a unique identifier for a datarun
-    df.index = pd.MultiIndex.from_tuples([(get_runID(x[0]), x[1]) for x in df.index])
-    # #prints information about the dataframe
+    df['temp']=df['temp'].apply(lambda x:x[0])
+    df['chip']= df['temp'].apply(lambda x:int(x[3:6]) )
+    df['device']= df['temp'].apply(lambda x:x[3:9] )
+    df['deptime']=df['temp'].apply(find_deptime)
+    df['fabstep'] = df['temp'].apply(find_fabstep)
+    df['parameters'] = df['temp'].apply(find_parameters)
+    df['multi'] = df['temp'].apply(find_numsweeps)
+    df['gate'] = df['temp'].apply(find_gate)
+    df['timestamp']=df['temp'].apply(get_timestamp)
+    df['uuid']=df['temp'].apply(get_runID)
+    df['fname']=df['temp']
+    df.drop(['temp'],axis=1,inplace=True)
+    # resets the index to a unique identifier for a datarun
+    df.reset_index(drop=True,inplace=True)
+    #prints information about the dataframe
     if v:
         print("dataframe after load_to_dataframe()")
         get_info(df)
@@ -183,6 +201,14 @@ def filter_data(df,column=["parameters",'multi'],tags=['VG10VDS0.01',1]):
             total_mask=total_mask & mask_data(df, column[i], tags[i])
     data=data[total_mask]
     return data
+def filter_fnames(df,search,remove=True):
+    """removes data where the filenames include the search string"""
+    data=df
+    if remove:
+        data=data[~data['fname'].str.contains("{}".format(search))]
+    else:
+        data=data[data['fname'].str.contains("{}".format(search))]
+    return data
 def match_data(df,column='fabstep',match='postSU8'):
     """filters the data by all runs on devices that have the match characteristic
     in their parameter column"""
@@ -190,25 +216,34 @@ def match_data(df,column='fabstep',match='postSU8'):
     ls=data[data[column].isin([match])]['device']
     data=data[data['device'].isin(ls)]
     return data
-def tile_data(df,tile="device",color="fabstep",show=True,save=False,v=False):
+
+def tile_data(df, column="device",row=None, colwrap=2, color="fabstep", show=True, save=False, v=False,  xlim="", ylim="", sharey=True):
     #Seaborn plotting example
     if v:
         print("dataframe pre tile_data()")
         get_info(df)
-    sns.set(style="ticks",palette='hls')
+    sns.set(style="ticks",font_scale=1,palette=divergent)
     # Initialize a grid of plots with an Axes for each walk
-    grid = sns.FacetGrid(df, col=tile, hue=color, col_wrap=2, size=3)
+    grid = sns.FacetGrid(df, col=column, hue=color,row=row, col_wrap=colwrap, size=4,sharey=sharey)
     # Draw a line plot to show the trajectory of each random walk
     grid.map(plt.semilogy, "VG", "ID").add_legend()
     # Adjust the tick positions and labels
-    grid.set(xlim=(-10, 10), ylim=(1e-11, 1e-7))
+    if xlim=='':
+        pass
+    else:
+        grid.set(xlim=xlim)
+    if ylim=='':
+        pass
+    else:
+        grid.set(ylim=ylim)
     # Adjust the arrangement of the plots
     grid.fig.tight_layout(w_pad=1)
 
     if show:
         plt.show()
     if save:
-        plt.savefig
+        grid.fig.suptitle(save,y=1.05)
+        grid.savefig("{}.png".format(save))
     plt.close()
 
 
@@ -233,19 +268,19 @@ if __name__ == "__main__":
     parser.add_argument("--search", help="Search string to filter the filenames by.",action="store")
     args = parser.parse_args()
     if args.function=="tile":
-        df=load_to_dataframe(args.directory,v=args.verbose)
+        df=pd.concat([load_to_dataframe(directory,v=args.verbose) for directory in args.directory])
 
         if args.interactive:
             embed()
         else:
             df=filter_data(df)
             df=match_data(df)
-            tile_data(df,v=args.verbose)
+            tile_data(df,v=args.verbose,show=args.show,save=args.save)
 
     if args.function=="plotall":
         assert len(args.directory)==1, "this function takes only one directory"
         for i in range(args.start,args.stop+1):
-            plotchip(args.directory[0], i,save=args.s,show=args.d,v=args.verbose)
+            plotchip(args.directory[0], i,save=args.save,show=args.show,v=args.verbose)
     if args.function=="collect":
         assert len(args.directory)==1, "this function takes only one directory"
         plot_all(args.directory[0],args.start,args.stop,args.search, save=args.s,show=args.d)
