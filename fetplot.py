9dd355f5eae9ac32c1ddc7d408fdeb5a3d9872a2
Leo Browning <leobrowning92@gmail.com>
2017-08-08 08:54:09 +1200

    added tile presets and cleaned up some spacing

--- fetplot.py
+++ fetplot.py
@@ -20,10 +20,14 @@ matplotlib.use("TkAgg")
 
 ############## COLORS ###################################
 
-divergent=sns.color_palette(["#3778bf", "#feb308", "#441830", "#c13838", "#2f6830"])
+divergent=sns.color_palette(["#3778bf", "#feb308", "#7F2C5A", "#c13838", "#2f6830"])
 hls=sns.color_palette("hls", 8)
+
+
 ##############   Helper Functions   #####################
 # Functions for pulling important information out of filename strings
+
+
 def find_deptime(fname):
     """returns the deptime in minutes from the filename"""
     if 'hrdep' in fname:
@@ -31,15 +35,13 @@ def find_deptime(fname):
     elif 'mindep' in fname:
         return int(re.search('(\d{1,3})mindep', fname, flags=re.IGNORECASE).group(1))
     else: return "deptimeERROR"
-
 def find_parameters(fname):
     try:
         if 'transfer' in fname:
-            return re.search('(VG[0-9]\d*(:?\.\d+)?VDS[0-9]\d*(:?\.\d+)?)',fname).group(1)
+            return re.search('(VG[0-9]\d*(:?\.\d+)?VDS[0-9]\d*(:?\.\d+)?)', fname).group(1)
         if 'output' in fname:
-            return re.search('(VDS[0-9]\d*(:?\.\d+)?VG[0-9]\d*(:?\.\d+)?)',fname).group(1)
+            return re.search('(VDS[0-9]\d*(:?\.\d+)?VG[0-9]\d*(:?\.\d+)?)', fname).group(1)
     except: return "parameterERROR"
-
 def find_fabstep(fname):
     try:return re.search('dep(.[^_]*)',fname).group(1)
     except:return "fabstepERROR"
@@ -52,7 +54,6 @@ def find_sweep(fname):
 def find_numsweeps(fname):
     try:return re.search('x(\d*)',fname).group(1)
     except: return 1
-
 def get_info(df):
     print("dataframe contains {} datapoints across {} runs".format(df.shape[0],len(set(df['uuid']))))
     print("the columns are: \n{}".format(list(df)))
@@ -65,16 +66,24 @@ def get_timestamp(fname):
 def find_gate(fname):
     try:return re.search('_([^_]*gate[^_]*)_', fname).group(1)
     except: return "backgate"
+def load_AFMdensities(directory='AFM_densities'):
+    fnames = glob.glob(os.path.join(directory,"*"))
+    densities={os.path.basename(x)[7:13]:float(pd.read_csv(x)['1uMean'][0]) for x in fnames}
+    deviations ={os.path.basename(x)[7:13]:float(pd.read_csv(x)['1uStd'][0]) for x in fnames}
+    return densities,deviations
+def dictsub(x,dictionary):
+    return dictionary[x]
 
+############### Plotting Functions ######################
 
-def plotchips(directory,v=True,show=True,save=True):
+def plotchips(directory,v=True,show=True,save=True,force=False):
     """
     This function simply plots all data from a chip, and whacks it in seperate
     plots inside a figure. The figures are saved individually in the
     /plots subdir of the parent directory.
     """
     checkdir(os.path.join(directory,"plots"))
-    df=load_to_dataframe(directory,v=v)
+    df=load_to_dataframe(directory,v=v,force=force)
     for n in set(df.chip):
         try:
             if v:
@@ -89,8 +98,7 @@ def plotchips(directory,v=True,show=True,save=True):
 
 
 
-
-####Tiling code
+##################### Tiling code ###########################
 def load_to_dataframe(directory,v=True,force=True):
     if force==False and os.path.isfile(os.path.join(directory,"dataframe.h5")):
         try:
@@ -107,16 +115,13 @@ def load_to_dataframe(directory,v=True,force=True):
             for i in range(1,len(directory)):
                 fnames=fnames+glob.glob(os.path.join(directory[i],"data/COL*"))
         fnames.sort()
-
+        mean,std=load_AFMdensities()
         # process fnames
         frames= [ pd.read_csv(fname) for fname in fnames ]
         # sets index to fname and concatenates dataset
-
         df=pd.concat(frames,keys=[os.path.basename(fname) for fname in fnames])
         # moves the fname to a column
         df['temp']=df.index
-
-
         #makes columns for the various parameters held in the filename
         df['temp']=df['temp'].apply(lambda x:x[0])
         df['chip']= df['temp'].apply(lambda x:int(x[3:6]) )
@@ -129,6 +134,8 @@ def load_to_dataframe(directory,v=True,force=True):
         df['sweep'] = df['temp'].apply(find_sweep)
         df['timestamp']=df['temp'].apply(get_timestamp)
         df['uuid']=df['temp'].apply(get_runID)
+        df['junctionMean']=df['device'].apply(lambda x: mean[x] if x in mean.keys() else np.nan)
+        df['junctionStd']=df['device'].apply(lambda x: std[x] if x in std.keys() else np.nan)
         df['fname']=df['temp']
         df.drop(['temp'],axis=1,inplace=True)
         # resets the index to a unique identifier for a datarun
@@ -141,6 +148,8 @@ def load_to_dataframe(directory,v=True,force=True):
         print("dataframe from {} after load_to_dataframe()".format(directory))
         get_info(df)
     return df
+
+################# Data Formatting functions
 
 def mask_data(df,column,tag):
     data=df
@@ -174,16 +183,24 @@ def match_data(df,column='fabstep',match='postSU8'):
     data=data[data['device'].isin(ls)]
     return data
 
-def tile_data(df, column="parameters",row='device', colwrap=None, color="fabstep", show=True, save=False, v=False,  xlim="", ylim="", sharey=True, x="VG", y="ID",log=True):
+def updownplot(x,y,**kwargs):
+    plt.semilogy(x[:len(x)//2],y[:len(y)//2],**kwargs)
+    plt.semilogy(x[len(x)//2:],y[len(y)//2:],'--',**kwargs,)
+
+
+############### Core Tiled data function ###########
+def tile_data(df, column="parameters",row='device', colwrap=None, color="fabstep", show=True, save=False, v=False,  xlim="", ylim="", sharey=True, x="VG", y="ID", log=True, updown=False, palette=divergent, col_order=None, hue_order=None):
     #Seaborn plotting example
     if v:
         print("dataframe pre tile_data()")
         get_info(df)
-    sns.set(style="ticks",font_scale=1,palette=divergent)
+    sns.set(style="ticks",font_scale=1,palette=palette)
     # Initialize a grid of plots with an Axes for each walk
-    grid = sns.FacetGrid(df, col=column, hue=color,row=row, col_wrap=colwrap, size=4,sharey=sharey)
+    grid = sns.FacetGrid(df, col=column, hue=color,row=row, col_wrap=colwrap, size=4,sharey=sharey, col_order=col_order, hue_order=hue_order)
     # Draw a line plot to show the trajectory of each random walk
-    if log:
+    if updown:
+        grid.map(updownplot, x, y).add_legend()
+    elif log and not(updown):
         grid.map(plt.semilogy, x, y).add_legend()
     else:
         grid.map(plt.plot, x, y).add_legend( )
@@ -208,6 +225,29 @@ def tile_data(df, column="parameters",row='device', colwrap=None, color="fabstep
 
 
 
+############## Tiling and data filtering presets ##############
+
+def select_20Vbackgates_oftopgated(df, plot=False, save="backgate_acrossFabsteps_updown"):
+    data = df[df.device.isin(set(df[df.fabstep=='postSU8'].device)) & (df.gate=='backgate') & (df.parameters=='VG20VDS0.1')]
+    if plot:
+        tile_data(data,  column='junctionMean', row=None, colwrap=2, sharey=True, updown=False, save=save,col_order= ['481_01','481_02','484_01','484_02','487_01','487_02','490_01','490_02','493_01','493_02',], hue_order=["postSD",'postSU8','postTop'])
+    else:
+        return data
+def topgate_spread(df,plot=False):
+    data = df[(df.fabstep=='postTop') & (df.parameters=='VG20VDS0.1')]
+    if plot:
+        tile_data(data, column='gate', row='device', color='gate', save='VG20VDS0.1_topgate_spread', sharey=False)
+    else:
+        return data
+def single_topgate(df,plot=False):
+    data = df[(df.fabstep=='postTop') & (df.parameters=='VG20VDS0.1') & (df.device=='490_02')]
+    if plot:
+        tile_data(data, column='device',row=None, color='gate', save='VG20VDS0.1_490_2_topgate', sharey=False)
+    else:
+        return data
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent("""\
@@ -216,7 +256,7 @@ if __name__ == "__main__":
                 collect   -  plots all data of a type specified by search on a single plot
                 tile      -  used with the -i flag to open an interactive session which allows exploration of the data"""))
 
-    parser.add_argument("function", type=str, choices=["plotall", "collect", "tile"],
+    parser.add_argument("function", type=str, choices=["plotall", "collect", "tile","test"],
         help="what plotting function to use: %(choices)s")
     parser.add_argument("directory",type=str, help="The directory containing the data folder to analyse",nargs='+')
 
@@ -246,7 +286,11 @@ if __name__ == "__main__":
 
     if args.function=="plotall":
         assert len(args.directory)==1, "this function takes only one directory"
-        plotchips(args.directory[0], save=args.save, show=args.show, v=args.verbose)
+        plotchips(args.directory[0], save=args.save, show=args.show, v=args.verbose,force=args.force)
     if args.function=="collect":
         assert len(args.directory)==1, "this function takes only one directory"
         plot_all(args.directory[0],args.start,args.stop,args.search, save=args.s,show=args.d)
+    if args.function=="test":
+        df=pd.concat([load_to_dataframe(directory, v=args.verbose, force=args.force) for directory in args.directory])
+        if args.interactive:
+            embed()
